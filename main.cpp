b833568199c2469b3fcbf3b1eecb7a4894d8b5cd
Bitchka98 <49394361+Bitchka98@users.noreply.github.com>
2019-09-25 14:48:25 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+# Rajzoloprog-C-
+Rajzoló program. A progi áll egy kordinata , teglalap, kör ,drawing és swg osztályból
+#include <iostream>
+#include "koord.h"
+#include "kör.h"
+
+using namespace std;
+
+int main()
+{
+    drawing d;
+    d.addkor();
+    d.addkor();
+    d.addkor();
+void saveToswg(d,"C.svg")
+    return 0;
+}
