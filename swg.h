bf219c89459c871ca63b48c10b07dfb9f3616a42
Bitchka98 <49394361+Bitchka98@users.noreply.github.com>
2019-09-25 14:51:41 +0200

    Create swg.h

---
+++ swg.h
@@ -0,0 +1,9 @@
+# Rajzoloprog-C-
+Rajzoló program. A progi áll egy kordinata , teglalap, kör ,drawing és swg osztályból
+#ifndef SWG_H
+#define SWG_H
+#include "kör.h"
+#include <string>
+
+void saveToswg(const drawing C,string filename);
+#endif // SWG_H
